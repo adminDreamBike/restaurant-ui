c34221a102f1253d68d631c938b730efc699869e
Carlos Rodriguez <carlos.dumar.con@greatminds.com>
2024-09-11 19:19:35 -0500

    restaurant ui fix bugs 2

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,17 @@
 
 import { Search } from "./components/Search/Search";
 import { DishList } from "./components/DishList/DishList";
-import {
-  Text,
-} from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start ">
-      <Text fontSize='3xl'>Carlos s Restaurant</Text>
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start ">
+        <Text fontSize="3xl">Carlos s Restaurant</Text>
         <Search />
         <DishList />
       </main>
+    </Suspense>
   );
 }
